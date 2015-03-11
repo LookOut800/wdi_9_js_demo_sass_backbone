6636bf08c188ceae98fc61120c095a6c5d9da8c6
fishermanswharff <fishermanswharff@github.com>
2015-03-10 21:51:50 -0400

    class demo ready

--- app/js/app.js
+++ app/js/app.js
@@ -1,25 +1,79 @@
-'use strict';
+function trace(){ for(var i = 0, count = arguments.length; i < count; i++){console.log(arguments[i]);}};
 
-var trace = function(){
-  for(var i = 0; i < arguments.length; i++){
-    console.log(arguments[i]);
+var Router = Backbone.Router.extend({
+  routes: {
+    '': 'home',
+    'home': 'home',
+    'comments': 'comments',
+    'new-submission': 'newSubmission'
+  },
+
+  home: function(){
+    $("#container").empty();
+    $.ajax({
+      url: 'http://jwhacker.herokuapp.com/submissions',
+      type: 'GET'
+    }).done(function(response){
+      var template = Handlebars.compile($("#homeTemplate").html());
+      $("#container").html(template({
+        submission: response
+      }));
+    }).fail(function(jqXHR, textStatus, errorThrown){
+      trace(jqXHR, textStatus, errorThrown);
+    });
+  },
+
+  comments: function(){
+    $("#container").empty();
+    $.ajax({
+      url: 'http://jwhacker.herokuapp.com/comments.json',
+      type: 'GET'
+    }).done(function(response){
+      var template = Handlebars.compile($("#commentTemplate").html());
+      $("#container").html(template({
+        comment: response
+      }));
+    }).fail(function(jqXHR, textStatus, errorThrown){
+      trace(jsXHR, textStatus, errorThrown)
+    });
+  },
+
+  newSubmission: function(){
+    $("#container").empty().load("partials/submission-form.html", function(response,status,xhr){
+      var $form = $("#new-submission-form");
+      $form.on("submit", function(e){
+        Submission.processForm(e,$form,router);
+      })
+    });
   }
-};
-
-var App = App || {};
-
-App.square = function(x){
-  return x * x
-};
-
-App.cube = function(x){
-  return x * x * x
-};
-
-App.greet = function(string){
-  return string = typeof string !== 'undefined' ? 'Hello ' + string : "Hello World";
-};
-
-$(document).ready(function(){
-  trace('hello world');
 });
+
+var router = new Router();
+Backbone.history.start();
+
+$(document).ajaxStart(function(e){
+  trace(e, "starting an ajax request");
+  $('section#ajax-preloader').fadeIn();
+  $('section#container').fadeOut();
+});
+
+$(document).ajaxComplete(function(event, xhr, settings) {
+  /* executes whenever an AJAX request completes */
+  $('section#ajax-preloader').fadeOut();
+  $('section#container').fadeIn();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
